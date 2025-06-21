acb09cdfcc2e4e0793df9554204f38c7c6939853
mikezhylka <applezhylka@gmail.com>
2025-06-21 12:02:34 +0200

    feat: add solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,58 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  beforeEach(() => {
+const fillInput = (selector, value) => {
+  cy.get(selector).should("be.visible");
+  cy.get(selector).type(value);
+  cy.get(selector).should("have.value", value);
+};
 
+const clickSubmit = () => {
+  cy.get(`button[type="submit"]`)
+    .should("be.visible")
+    .should("not.be.disabled")
+    .click();
+};
+
+describe("Sign In page", () => {
+  beforeEach(() => {
+    cy.visit("/login"); // baseUrl + /login
+    cy.url().should("contain", "/login");
   });
 
-  it('', () => {
+  it("should allow to fill username", () => {
+    fillInput("#username", "tomsmith");
+  });
 
+  it("should allow to fill password", () => {
+    fillInput("#password", "SuperSecretPassword!");
+  });
+
+  it("should allow to login with correct credentials", () => {
+    fillInput("#username", "tomsmith");
+    fillInput("#password", "SuperSecretPassword!");
+
+    clickSubmit();
+
+    cy.get("#flash.flash.success", { timeout: 5000 }).should("be.visible");
+  });
+
+  it("should allow to logout", () => {
+    fillInput("#username", "tomsmith");
+    fillInput("#password", "SuperSecretPassword!");
+
+    clickSubmit();
+
+    cy.get("#flash.flash.success", { timeout: 5000 }).should("be.visible");
+    cy.get(`a[href="/logout"]`).should("be.visible").click();
+    cy.url().should("contain", "/login");
+  });
+
+  it("should restrict login with wrong credentials", () => {
+    fillInput("#username", "tomsmither");
+    fillInput("#password", "SuperUnsecretPassword!");
+
+    clickSubmit();
+
+    cy.get("#flash.flash.error", { timeout: 5000 }).should("be.visible");
   });
 });
